2b821c597b2cd0980561d0d6b37a49a8e2b34953
robertaskarov <robert.askarov43@gmail.com>
2025-08-14 12:40:19 +0300

    add eslint and settings

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,26 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      // Общие "чистые" правила кода
+      'prefer-spread': 'warn',      // Предпочитать [...arr] вместо apply()
+      'prefer-rest-params': 'warn', // Предпочитать ...args вместо arguments
+      'prefer-const': 'warn',       // Использовать const если переменная не меняется
+      'prefer-arrow-callback': 'warn', // Предпочитать стрелочные функции в коллбеках
+      'no-var': 'error',            // Запрещён var
+      'no-eq-null': 'error',        // Запрещён == null (только === null)
+      'no-else-return': ['error', { allowElseIf: false }], // После return не ставим else
+      eqeqeq: 'error',              // Всегда использовать === вместо ==
+      'dot-notation': 'error',      // obj.key вместо obj['key']
+      'dot-location': ['error', 'property'], // Точка ставится перед свойством
+      curly: 'error',               // Всегда использовать {}
+      'no-inner-declarations': 'error', // Запрещено объявлять функции внутри блоков
+      'no-extra-boolean-cast': 'error',  // !!true → лишнее
+      'no-empty': 'error',          // Запрещены пустые блоки {}
+      quotes: ['error', 'single'],  // Одинарные кавычки
+      'no-nested-ternary': 'error', // Запрещены вложенные тернарники
+      'no-console': 'warn',         // console.log разрешён, но предупреждение
+      'no-debugger': 'warn',        // debugger разрешён, но предупреждение
+    }
   },
 ])
