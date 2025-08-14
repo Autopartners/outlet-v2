22ce15e85552d997c7ca8f3db202873f232c015f
robertaskarov <robert.askarov43@gmail.com>
2025-08-14 17:55:55 +0300

    change img src

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -9,7 +9,7 @@ export const HomePage = () => {
   return (
     <Container p={0} fluid>
       <Box pos="relative" h={550}>
-        <Image src="/public/road.png" w="100%" h="100%" style={{ zIndex: 1, objectFit: 'cover' }} />
+        <Image src="/road.png" w="100%" h="100%" style={{ zIndex: 1, objectFit: 'cover' }} />
         <Box pos="absolute" top="50%" left="50%" style={{ transform: 'translate(-50%, -50%)', textAlign: 'center', zIndex: 2 }}>
           <Text c="white" fz={40} mb="md">
             Добро пожаловать на аукцион CarsOutlet!
