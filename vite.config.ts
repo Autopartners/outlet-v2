7d0611dadf83e6552940dfc84cd1186f2a6cc864
robertaskarov <robert.askarov43@gmail.com>
2025-08-14 16:04:20 +0300

    add alias absolute path and types node for typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   server: { port: 3000 },
   build: { outDir: 'build' },
   optimizeDeps: {
-    include: [
-      '@mantine/core',
-      '@mantine/hooks',
-      '@mantine/modals',
-      '@mantine/notifications',
-      '@tabler/icons-react'
-    ]
+    include: ['@mantine/core', '@mantine/hooks', '@mantine/modals', '@mantine/notifications', '@tabler/icons-react']
   }
 });
