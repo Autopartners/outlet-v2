7663a5d315fcf07985fdf1ee671729d3648bfa81
MikhailAG <mikhail.gubanov.a@gmail.com>
2025-08-14 13:11:08 +0300

    eslint rules update

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,9 @@ import tseslint from 'typescript-eslint';
 import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['build']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -39,7 +39,36 @@ export default tseslint.config([
       quotes: ['error', 'single'], // Одинарные кавычки
       'no-nested-ternary': 'error', // Запрещены вложенные тернарники
       'no-console': 'warn', // console.log разрешён, но предупреждение
-      'no-debugger': 'warn' // debugger разрешён, но предупреждение
+      'no-debugger': 'warn', // debugger разрешён, но предупреждение
+
+      // Правила для пробелов и отступов
+      'no-multi-spaces': 'error', // Запрещает множественные пробелы
+      'no-trailing-spaces': 'error', // Запрещает пробелы в конце строк
+      'no-whitespace-before-property': 'error', // Запрещает пробелы перед точкой в свойствах
+      'space-in-parens': ['error', 'never'], // Нет пробелов внутри скобок
+      'array-bracket-spacing': ['error', 'never'], // Нет пробелов внутри квадратных скобок
+      'block-spacing': ['error', 'always'], // Пробелы внутри блоков
+      'comma-spacing': ['error', { 'before': false, 'after': true }], // Пробелы после запятых
+      'keyword-spacing': ['error', { 'before': true, 'after': true }], // Пробелы вокруг ключевых слов
+      'func-call-spacing': ['error', 'never'], // Нет пробелов при вызове функций
+      'space-before-blocks': 'error', // Пробел перед блоком
+      'space-before-function-paren': ['error', {
+        'anonymous': 'never',
+        'named': 'never',
+        'asyncArrow': 'always'
+      }], // Пробелы перед скобками функций
+      'spaced-comment': ['error', 'always'], // Пробелы в комментариях
+      'indent': ['error', 2], // Отступы в 2 пробела
+      'semi-spacing': ['error', { 'before': false, 'after': true }], // Пробелы вокруг точек с запятой
+      'arrow-spacing': ['error', { 'before': true, 'after': true }], // Пробелы вокруг стрелки
+      'template-tag-spacing': ['error', 'never'], // Пробелы в теговых шаблонах
+      'space-infix-ops': 'error', // Пробелы вокруг операторов
+      'key-spacing': ['error', {
+        'beforeColon': false,
+        'afterColon': true
+      }], // Пробелы в свойствах объектов
+      'rest-spread-spacing': ['error', 'never'], // Пробелы в spread операторе
+      'no-mixed-spaces-and-tabs': 'error' // Запрещает смешивание пробелов и табов
     }
   }
 ]);
