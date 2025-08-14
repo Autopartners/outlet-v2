c4c36036908678ae537795e9963d9e805fc0bc7b
MikhailAG <mikhail.gubanov.a@gmail.com>
2025-08-14 10:30:30 +0300

    outlet deploy

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while getopts "p:" option; do
+  case $option in
+    p)
+      path=$OPTARG
+    ;;
+  esac
+done
+
+server='ap-user@192.168.1.211 -p 10322'
+keep_releases=6
+now=$(date +%Y%m%d%H%M)
+
+if [[ `git status --porcelain` ]]; then
+  echo 'Please commit your changes!'
+else
+  ssh $server "mkdir -p $path/$now"
+  ssh $server "source ~/.nvm/nvm.sh &&
+    cd $path/repo &&
+    git checkout HEAD -- package.json pnpm-lock.yaml &&
+    git pull &&
+    ~/.local/share/pnpm/pnpm update &&
+    NODE_OPTIONS=--max-old-space-size=4096 ~/.local/share/pnpm/pnpm assemble &&
+    cp -r ./build/* $path/$now &&
+    cd $path &&
+    ls -t | grep -v repo | sed -e '1,${keep_releases}d' | xargs -r -d '\n' rm -r &&
+    rm -f ./current &&
+    ln -s ./$now ./current &&
+    echo 'deployed to $now'"
+fi
