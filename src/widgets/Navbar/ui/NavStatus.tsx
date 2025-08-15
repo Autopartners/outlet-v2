c75dfdb372039d45f585a365d26c10c1b4ab329f
robertaskarov <robert.askarov43@gmail.com>
2025-08-15 14:02:35 +0300

    add navstatus

---
+++ src/widgets/Navbar/ui/NavStatus.tsx
@@ -0,0 +1,15 @@
+import { Avatar, UnstyledButton, Text, Flex } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import { useMe } from '@/app/providers/me/useMe';
+
+export const NavStatus = () => {
+  const { me } = useMe();
+  return (
+    <UnstyledButton component={Link} to={'/profile'}>
+      <Flex px={'sm'} gap={'sm'} align={'center'}>
+        <Avatar radius={'xl'} color={'blue'} />
+        <Text size={'md'}>{me.name}</Text>
+      </Flex>
+    </UnstyledButton>
+  );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,12 +2,13 @@ import { AppShell, Image, Group, UnstyledButton } from '@mantine/core';
 import { NavLink } from 'react-router';
 import { AppRouter } from '@/app/routers/appRouter.tsx';
 import { useNavigate } from 'react-router-dom';
+import { NavStatus } from '@/widgets/Navbar/ui/NavStatus';
 
 export function Navbar() {
   const nav = useNavigate();
 
   return (
-    <AppShell header={{ height: 60 }} padding="md">
+    <AppShell header={{ height: 70 }} padding="md">
       <AppShell.Header>
         <Group h="100%" px="md">
           <Group justify="space-between" style={{ flex: 1 }}>
@@ -15,18 +16,11 @@ export function Navbar() {
               <Image src={'/outlet_circle.png'} h={35} />
             </NavLink>
             <Group ml="xl" gap={50} p="md" visibleFrom="sm" fw={500}>
-              <UnstyledButton onClick={() => nav('/')}>
-                Home
-              </UnstyledButton>
-              <UnstyledButton onClick={() => nav('/lots')}>
-                Lots
-              </UnstyledButton>
-              <UnstyledButton onClick={() => nav('/about')}>
-                About
-              </UnstyledButton>
-              <UnstyledButton onClick={() => nav('/rules')}>
-                Rules
-              </UnstyledButton>
+              <UnstyledButton onClick={() => nav('/')}>Home</UnstyledButton>
+              <UnstyledButton onClick={() => nav('/lots')}>Lots</UnstyledButton>
+              <UnstyledButton onClick={() => nav('/about')}>About</UnstyledButton>
+              <UnstyledButton onClick={() => nav('/rules')}>Rules</UnstyledButton>
+              <NavStatus />
             </Group>
           </Group>
         </Group>
@@ -36,18 +30,10 @@ export function Navbar() {
       </AppShell.Main>
       <AppShell.Footer>
         <Group justify={'space-around'} p="md" hiddenFrom="sm" fw={500}>
-          <UnstyledButton onClick={() => nav('/')}>
-            Home
-          </UnstyledButton>
-          <UnstyledButton onClick={() => nav('/lots')}>
-            Lots
-          </UnstyledButton>
-          <UnstyledButton onClick={() => nav('/about')}>
-            About
-          </UnstyledButton>
-          <UnstyledButton onClick={() => nav('/rules')}>
-            Rules
-          </UnstyledButton>
+          <UnstyledButton onClick={() => nav('/')}>Home</UnstyledButton>
+          <UnstyledButton onClick={() => nav('/lots')}>Lots</UnstyledButton>
+          <UnstyledButton onClick={() => nav('/about')}>About</UnstyledButton>
+          <UnstyledButton onClick={() => nav('/rules')}>Rules</UnstyledButton>
         </Group>
       </AppShell.Footer>
     </AppShell>
