d035422d3ecbd72477a91483adc4e72c92764213
robertaskarov <robert.askarov43@gmail.com>
2025-08-15 19:25:24 +0300

    make navbar more adaptive

--- src/widgets/Navbar/ui/NavStatus.tsx
+++ src/widgets/Navbar/ui/NavStatus.tsx
@@ -1,14 +1,16 @@
 import { Avatar, UnstyledButton, Text, Flex } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import { useMe } from '@/app/providers/me/useMe';
+import { useApp } from '@/app/providers/app/useApp.ts';
 
 export const NavStatus = () => {
   const { me } = useMe();
+  const { isMobile } = useApp();
   return (
     <UnstyledButton component={Link} to="/profile">
       <Flex px="sm" gap="sm" align="center">
         <Avatar radius="xl" color="blue" />
-        <Text size="md">{me.name || me.username}</Text>
+        {!isMobile && <Text size="md">{me.name || me.username}</Text>}
       </Flex>
     </UnstyledButton>
   );
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -6,51 +6,58 @@ import { NavStatus } from '@/widgets/Navbar/ui/NavStatus';
 import { useMe } from '@/app/providers/me/useMe';
 import { connecturl } from '@/shared/lib/api';
 import { Link } from 'react-router-dom';
+import { useApp } from '@/app/providers/app/useApp.ts';
 
 export function Navbar() {
   const nav = useNavigate();
   const { pathname } = useLocation();
-  const { me } = useMe();
+  const { isMobile } = useApp();
+  const { me, loading } = useMe();
 
   return (
     <AppShell header={{ height: 70 }} padding="md">
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Group pos="relative" justify="space-between" style={{ flex: 1 }}>
-            <NavLink to="/">
-              <Image src="/outlet_circle.png" h={35} />
-            </NavLink>
-            <Group style={{ transform: 'translate(-50%, -50%)' }} pos="absolute" left="50%" top="50%" gap={50} visibleFrom="md" fw={500}>
-              <Button variant={pathname === '/' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/')}>
-                Home
-              </Button>
-              {me.id && (
-                <Button variant={pathname === '/lots' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/lots')}>
-                  Lots
+      {(!isMobile || !me.id || pathname === '/') && (
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Group pos="relative" justify="space-between" style={{ flex: 1 }}>
+              <NavLink to="/">
+                <Image src="/outlet_circle.png" h={35} />
+              </NavLink>
+              <Group style={{ transform: 'translate(-50%, -50%)' }} pos="absolute" left="50%" top="50%" gap={50} visibleFrom="md" fw={500}>
+                <Button variant={pathname === '/' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/')}>
+                  Home
                 </Button>
+                {me.id && (
+                  <Button variant={pathname === '/lots' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/lots')}>
+                    Lots
+                  </Button>
+                )}
+                <Button variant={pathname === '/about' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/about')}>
+                  About
+                </Button>
+                <Button variant={pathname === '/rules' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/rules')}>
+                  Rules
+                </Button>
+              </Group>
+              {me.id && !isMobile ? (
+                <NavStatus />
+              ) : (
+                !loading &&
+                !me.id && (
+                  <Flex gap={'md'}>
+                    <Button component={Link} variant={'outline'} to={`${connecturl}/login`}>
+                      Войти
+                    </Button>
+                    <Button component={Link} to={`${connecturl}/signup#outlet`}>
+                      Зарегистрироваться
+                    </Button>
+                  </Flex>
+                )
               )}
-              <Button variant={pathname === '/about' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/about')}>
-                About
-              </Button>
-              <Button variant={pathname === '/rules' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/rules')}>
-                Rules
-              </Button>
             </Group>
-            {me.id ? (
-              <NavStatus />
-            ) : (
-              <Flex gap={'md'}>
-                <Button component={Link} variant={'outline'} to={`${connecturl}/login`}>
-                  Войти
-                </Button>
-                <Button component={Link} to={`${connecturl}/signup#outlet`}>
-                  Зарегистрироваться
-                </Button>
-              </Flex>
-            )}
           </Group>
-        </Group>
-      </AppShell.Header>
+        </AppShell.Header>
+      )}
       <AppShell.Main p={0}>
         <AppRouter />
       </AppShell.Main>
@@ -67,9 +74,7 @@ export function Navbar() {
           <Button variant={pathname === '/about' ? 'light' : 'subtle'} size="sm" color="black" onClick={() => nav('/about')}>
             About
           </Button>
-          <Button variant={pathname === '/rules' ? 'light' : 'subtle'} size="sm" color="black" onClick={() => nav('/rules')}>
-            Rules
-          </Button>
+          {me.id && <NavStatus />}
         </Group>
       </AppShell.Footer>
     </AppShell>
