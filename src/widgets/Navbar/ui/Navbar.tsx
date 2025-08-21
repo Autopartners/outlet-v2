70b630028db3db5d7a8549af7c2f5a97e26dff76
robertaskarov <robert.askarov43@gmail.com>
2025-08-21 19:01:36 +0300

    change headers and add icons for mobile in navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -6,13 +6,14 @@ import { NavStatus } from '@/widgets/Navbar/ui/NavStatus';
 import { useMe } from '@/app/providers/me/useMe';
 import { useApp } from '@/app/providers/app/useApp.ts';
 import { AuthButtons } from '@/widgets/Navbar/ui/AuthButtons';
+import { IconCar, IconHome, IconInfoCircle } from '@tabler/icons-react';
 
 export function Navbar() {
   const nav = useNavigate();
   const { pathname } = useLocation();
   const { isMobile } = useApp();
   const { me, loading } = useMe();
-  const lotsMobilePage = pathname === '/lots' && isMobile
+  const lotsMobilePage = pathname === '/lots' && isMobile;
 
   return (
     <AppShell header={{ height: lotsMobilePage ? 0 : 70 }} footer={{ height: isMobile ? 70 : 0 }} padding="md">
@@ -23,20 +24,48 @@ export function Navbar() {
               <NavLink to="/">
                 <Image src="/outlet_circle.png" h={35} />
               </NavLink>
-              <Group style={{ transform: 'translate(-50%, -50%)' }} pos="absolute" left="50%" top="50%" gap={50} visibleFrom="md" fw={500}>
-                <Button variant={pathname === '/' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/')}>
-                  Home
+              <Group
+                style={{ transform: 'translate(-50%, -50%)' }}
+                pos="absolute"
+                left="50%"
+                top="50%"
+                gap={50}
+                visibleFrom="md"
+                fw={500}
+              >
+                <Button
+                  variant={pathname === '/' ? 'light' : 'subtle'}
+                  size="md"
+                  color="black"
+                  onClick={() => nav('/')}
+                >
+                  Главная
                 </Button>
                 {me.id && (
-                  <Button variant={pathname.includes('/lots') ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/lots')}>
-                    Lots
+                  <Button
+                    variant={pathname.includes('/lots') ? 'light' : 'subtle'}
+                    size="md"
+                    color="black"
+                    onClick={() => nav('/lots')}
+                  >
+                    Лоты
                   </Button>
                 )}
-                <Button variant={pathname === '/about' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/about')}>
-                  About
+                <Button
+                  variant={pathname === '/about' ? 'light' : 'subtle'}
+                  size="md"
+                  color="black"
+                  onClick={() => nav('/about')}
+                >
+                  О компании
                 </Button>
-                <Button variant={pathname === '/rules' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/rules')}>
-                  Rules
+                <Button
+                  variant={pathname === '/rules' ? 'light' : 'subtle'}
+                  size="md"
+                  color="black"
+                  onClick={() => nav('/rules')}
+                >
+                  Правила
                 </Button>
               </Group>
               {me.id && !isMobile ? <NavStatus /> : <AuthButtons isMobile={isMobile} me={me} loading={loading} />}
@@ -54,15 +83,25 @@ export function Navbar() {
       <AppShell.Footer>
         <Group justify="space-around" p="md" hiddenFrom="md" fw={500}>
           <Button variant={pathname === '/' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/')}>
-            Home
+            <IconHome size={30} />
           </Button>
           {me.id && (
-            <Button variant={pathname.includes('/lots') ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/lots')}>
-              Lots
+            <Button
+              variant={pathname.includes('/lots') ? 'light' : 'subtle'}
+              size="md"
+              color="black"
+              onClick={() => nav('/lots')}
+            >
+              <IconCar size={30} />
             </Button>
           )}
-          <Button variant={pathname === '/about' ? 'light' : 'subtle'} size="md" color="black" onClick={() => nav('/about')}>
-            About
+          <Button
+            variant={pathname === '/about' ? 'light' : 'subtle'}
+            size="md"
+            color="black"
+            onClick={() => nav('/about')}
+          >
+            <IconInfoCircle size={30} />
           </Button>
           {me.id && <NavStatus />}
         </Group>
