458f4549660fc9a8f0189fdda6f900377194f4c9
robertaskarov <robert.askarov43@gmail.com>
2025-09-29 19:53:14 +0300

    Добавление табличного вида для просмотра лотов.

--- src/pages/lots/index/ui/lotsList.tsx
+++ src/pages/lots/index/ui/lotsList.tsx
@@ -1,16 +1,36 @@
-import { Box, Container, Flex, SimpleGrid, ThemeIcon, Text, Alert } from '@mantine/core';
+import {
+  Box,
+  Container,
+  Flex,
+  SimpleGrid,
+  ThemeIcon,
+  Text,
+  Alert,
+  ActionIcon,
+  Stack,
+  Button,
+  Popover, Tooltip, NumberInput, Loader as MantineLoader
+} from '@mantine/core';
 import { useLots } from '@/pages/lots/index/api/useLots.ts';
 import type { Lot } from '@/entities/lot';
 import { useSearchParams } from 'react-router-dom';
 import { LotCard } from '@/pages/lots/index/ui/lotCard.tsx';
 import { LotPages } from '@/pages/lots/index/ui/lotPages.tsx';
 import { LotsListSkeletonLoader } from '@/pages/lots/index/ui/skeletons/lotsListSkeletonLoader.tsx';
-import { IconHourglassHigh, IconHourglassLow } from '@tabler/icons-react';
+import { IconCurrencyRubel, IconHourglassHigh, IconHourglassLow, IconTable } from '@tabler/icons-react';
 import { useApp } from '@/app/providers/app/useApp';
+import { useState } from 'react';
+import { IconLayoutGridFilled } from '@tabler/icons-react';
+import { useBid } from '@/pages/lots/show/api/useBid.ts';
+import { LotsTableSkeletonLoader } from '@/pages/lots/index/ui/skeletons/lotsTableSkeletonLoader.tsx';
 
 export const LotsList = () => {
   const [searchParams] = useSearchParams();
   const { isMobile } = useApp();
+  const [openedLotId, setOpenedLotId] = useState<number | null>(null);
+  const [bid, setBid] = useState<string | number | undefined>('');
+  const { bidMutation } = useBid();
+  const [activeView, setActiveView] = useState('cards');
   const { lots, isLoading, pages, refetch } = useLots({
     page: searchParams.get('page') || '1',
     per_page: '12',
@@ -25,7 +45,9 @@ export const LotsList = () => {
   });
 
   if (isLoading || !lots) {
-    return <LotsListSkeletonLoader />;
+    return activeView === 'cards'
+      ? <LotsListSkeletonLoader />
+      : <LotsTableSkeletonLoader />;
   }
 
   if (lots.length === 0) {
@@ -56,13 +78,114 @@ export const LotsList = () => {
           </Flex>
         </Flex>
       </Alert>
-      <SimpleGrid spacing={30} mt={20} cols={{ lg: 3, sm: 1 }}>
-        {lots.map((lot: Lot) => (
-          <Box key={lot.id}>
-            <LotCard lot={lot} maxPhotos={5} refetchLots={refetch} />
-          </Box>
-        ))}
-      </SimpleGrid>
+      <Flex justify="flex-end" gap={5} mt={10}>
+        <ActionIcon
+          onClick={() => setActiveView('table')}
+          variant={activeView === 'table' ? 'filled' : 'subtle'}
+          size="xl"
+          c={activeView === 'table' ? 'white' : 'dark'}
+        >
+          <IconTable size={32} />
+        </ActionIcon>
+        <ActionIcon
+          onClick={() => setActiveView('cards')}
+          variant={activeView === 'cards' ? 'filled' : 'subtle'}
+          size="xl"
+          c={activeView === 'cards' ? 'white' : 'dark'}
+        >
+          <IconLayoutGridFilled size={32} />
+        </ActionIcon>
+      </Flex>
+      {activeView === 'cards' &&
+        <SimpleGrid spacing={30} mt={20} cols={{ lg: 3, sm: 1 }}>
+          {lots.map((lot: Lot) => (
+            <Box key={lot.id}>
+              <LotCard lot={lot} maxPhotos={5} refetchLots={refetch} />
+            </Box>
+          ))}
+        </SimpleGrid>
+      }
+      {activeView === 'table' &&
+        <Stack mt={50} gap={20}>
+          {lots.map((lot: Lot) => (
+            <Flex justify="space-between" align="center">
+              <Text fz={16}>{lot.code}</Text>
+              <Stack gap={0} w={450}>
+                <Text c="blue" fw="bold">{lot.definition_name}</Text>
+                <Flex justify="space-between" w={300}>
+                  <Text>{lot.vehicle_year_of_production}</Text>
+                  <Text>{lot.city_of_remarketing_name}</Text>
+                  <Text>{Number(lot.return_km)}</Text>
+                </Flex>
+              </Stack>
+              <Box w={150}>
+                <Tooltip label="Моя ставка">
+                  <Flex align="center" justify="start">
+                    {lot.my_bid && <IconCurrencyRubel stroke={2} size={20} />}
+                    <Text fz={18} fw="bold">{lot.my_bid}</Text>
+                  </Flex>
+                </Tooltip>
+              </Box>
+              <Popover
+                width={200}
+                position="bottom"
+                opened={openedLotId === lot.id}
+                onChange={(o) => setOpenedLotId(o ? lot.id : null)}
+                withArrow
+                shadow="md"
+              >
+                <Popover.Target>
+                  <Button
+                    color="green"
+                    onClick={() => setOpenedLotId(openedLotId === lot.id ? null : lot.id)}
+                  >
+                    Сделать
+                    ставку
+                  </Button>
+                </Popover.Target>
+                <Popover.Dropdown w={250}>
+                  {lot.stage === 'second_stage' && (
+                    <Tooltip label="Максимально предложенная сумма из первого этапа">
+                      <Flex direction="column" align="flex-start">
+                        <Text fz={16}>Текущая ставка</Text>
+                        <Text fz={18} fw="bold" c="red.9">
+                          {lot.second_stage_minimal_price?.toLocaleString('ru-RU') || 0} ₽
+                        </Text>
+                      </Flex>
+                    </Tooltip>
+                  )}
+                  <NumberInput
+                    max={100000000}
+                    mt="xs"
+                    size="md"
+                    placeholder="Ставка"
+                    allowDecimal={false}
+                    allowNegative={false}
+                    thousandSeparator={' '}
+                    value={bid}
+                    step={100000}
+                    onChange={setBid}
+                  />
+                  <Button
+                    mt={10}
+                    w="100%"
+                    onClick={() => {
+                      bidMutation.mutate({ value: bid, lot_id: String(lot.id) });
+                      setOpenedLotId(null);
+                    }}
+                    color="green.7"
+                    disabled={!bid || Number(bid) < (lot.second_stage_minimal_price ?? 0)}
+                    leftSection={bidMutation.status === 'pending' && <MantineLoader type="dots" color="white" />}
+                  >
+                    Сохранить
+                  </Button>
+                </Popover.Dropdown>
+              </Popover>
+            </Flex>
+          ))
+          }
+        </Stack>
+      }
       <LotPages pages={pages} pos="bottom" />
     </Container>);
 };
