5f135ad9d3bb9428da4c543ee377b62e63905913
robertaskarov <robert.askarov43@gmail.com>
2025-10-07 16:18:23 +0300

    remove fetch autoteka

--- src/pages/lots/show/ui/autotekaInfoPage.tsx
+++ src/pages/lots/show/ui/autotekaInfoPage.tsx
@@ -1,21 +1,6 @@
 import { Box, Flex, Card } from '@mantine/core';
 
 export const AutotekaInfoPage = () => {
-  const params = new URLSearchParams();
-  params.append('grant_type', 'client_credentials');
-  params.append('client_id', 'ВАШ_CLIENT_ID');
-  params.append('client_secret', 'ВАШ_CLIENT_SECRET');
-
-  fetch('https://pro.autoteka.ru/token/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: params.toString()
-  })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
   return (
     <Flex justify="center">
       <Card w="80%" withBorder>
