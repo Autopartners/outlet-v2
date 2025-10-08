7769b6fd94079d1c15020f74842695f06bbcb14d
MikhailAG <mikhail.gubanov.a@gmail.com>
2025-10-08 12:58:07 +0300

    fix home loader

--- src/pages/home/ui/HomeSkeletonLoader.tsx
+++ src/pages/home/ui/HomeSkeletonLoader.tsx
@@ -6,8 +6,8 @@ export const HomeSkeletonLoader = ({ ...props }) => {
 
   return (
     <SimpleGrid spacing={30} mt={20} cols={{ lg: 3, sm: 1 }} w={isMobile ? '90%' : '70%'} mx="auto" {...props}>
-      <Skeleton w={isMobile ? '100%' : 550} h={490} radius="md"/>
-      <Skeleton w={isMobile ? '100%' : 550} h={490} radius="md"/>
-      <Skeleton w={isMobile ? '100%' : 550} h={490} radius="md"/>
+      <Skeleton w="100%" h={490} radius="md"/>
+      <Skeleton w="100%" h={490} radius="md"/>
+      <Skeleton w="100%" h={490} radius="md"/>
     </SimpleGrid>);
 }
