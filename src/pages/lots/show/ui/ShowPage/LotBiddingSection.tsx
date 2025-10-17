bf0a3def3e812a99fa082488ccf0f71e77faaab3
robertaskarov <robert.askarov43@gmail.com>
2025-10-17 16:57:08 +0300

    change view of stages in lot page

--- src/pages/lots/show/ui/ShowPage/LotBiddingSection.tsx
+++ src/pages/lots/show/ui/ShowPage/LotBiddingSection.tsx
@@ -1,8 +1,9 @@
-import { Box, Card, Flex, Grid, Stack, Tooltip, Text, Badge } from '@mantine/core';
+import { Box, Card, Flex, Grid, Stack, Tooltip, Text, Badge, ThemeIcon } from '@mantine/core';
 import { useApp } from '@/app/providers/app/useApp.ts';
 import { MakeBidInput } from '@/shared/ui/LotOperations/MakeBid/MakeBidInput.tsx';
 import type { Lot } from '@/entities/lot';
 import { stageStrings } from '@/shared/lib/constants';
+import { IconPercentage33 } from '@tabler/icons-react';
 
 export const LotBiddingSection = ({ lot }: {lot: Lot}) => {
   const { isMobile } = useApp();
@@ -12,14 +13,17 @@ export const LotBiddingSection = ({ lot }: {lot: Lot}) => {
 
   return (
     <Grid.Col span={{ base: 12, md: 5 }}>
-      <Card radius="lg" bg="blue.9" p={10}>
-        <Text ta="center" fz={isMobile ? 18 : 25} c="white">
-          {stageStrings[lot.stage as keyof typeof stageStrings]}
-        </Text>
-      </Card>
       <Box mt={20}>
         <Card radius="lg">
           <Stack>
+            <Card bg="gray.2">
+              <Flex justify="center" align="center" gap={10}>
+                <ThemeIcon variant="transparent" c="blue.7"><IconPercentage33 size={20} /></ThemeIcon>
+                <Text ta="left" fz={isMobile ? 18 : 25} c="black">
+                  {stageStrings[lot.stage as keyof typeof stageStrings]}
+                </Text>
+              </Flex>
+            </Card>
             {lot.stage === 'second_stage' && (
               <Tooltip label="Максимально предложенная сумма из первого этапа">
                 <Flex justify="space-between" align="flex-end">
@@ -37,7 +41,7 @@ export const LotBiddingSection = ({ lot }: {lot: Lot}) => {
                 <Flex justify="space-between" align="center">
                   <Flex align="center" gap={10}>
                     <Text fz={18}>Моя ставка</Text>
-                    <Badge>1 этап</Badge>
+                    <Badge variant="light">1 этап</Badge>
                   </Flex>
                   <Text fz={25} fw="bold" c="blue.9">
                     {lot.my_first_stage_amount.toLocaleString('ru-RU')}
@@ -52,7 +56,7 @@ export const LotBiddingSection = ({ lot }: {lot: Lot}) => {
                 <Flex justify="space-between" align="center">
                   <Flex align="center" gap={10}>
                     <Text fz={18}>Моя ставка</Text>
-                    <Badge>2 этап</Badge>
+                    <Badge variant="light">2 этап</Badge>
                   </Flex>
                   <Text fz={25} fw="bold" c="blue.9">
                     {lot.my_second_stage_amount.toLocaleString('ru-RU')}
